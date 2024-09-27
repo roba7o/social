a5752eb911e7537db9c4b77797b2831d3259a27f
roba7o <rhm14148@gmail.com>
2024-09-27 18:22:58 +0100

    password change and password reset htmls and urls added

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,9 +5,22 @@ from . import views
 urlpatterns = [
     # Previous log-in url
     # path('login/', views.user_login, name='login'),
+
     # Login / Logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Change password urls
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password-change'
+    ),
+    path(
+        'password-change/done',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
 
     path('', views.dashboard, name = 'dashboard'),
 ]
