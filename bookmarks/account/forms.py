f1acecc336d8497799123cb5dc7464be1bc7a34e
roba7o <rhm14148@gmail.com>
2024-10-08 08:55:34 +0100

    UserRegistrationForm added

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+
+    password2 = forms.CharField(
+        label='Repeat Password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
